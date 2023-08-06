d9830fc8a29caee4d420eec76f4e7a1b0ffccee1
h-bails <>
2023-08-06 14:13:55 +0000

    Set debug to true

--- tubular/settings.py
+++ tubular/settings.py
@@ -32,7 +32,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-h-bails-pp5-tr09s2fklw.us2.codeanyapp.com',
