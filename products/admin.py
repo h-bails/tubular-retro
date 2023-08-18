f0648ab4a068f3b39fe196cf23a144e635ca0751
h-bails <>
2023-08-18 12:12:23 +0000

    Customise admin view

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,31 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'measurements',
+        'condition',
+        'price',
+        'sku',
+        'category',
+        'image_1',
+        'image_1_url',
+    )
+
+    ordering = ('date_added',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+    )
+    
+    ordering = ('name',)
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
