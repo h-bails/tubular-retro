b423aca05f3fc97bb9646f92002831ce4ba0e9ce
h-bails <>
2023-08-19 12:29:08 +0000

    Add category filtering

--- products/views.py
+++ products/views.py
@@ -15,10 +15,10 @@ def all_products(request):
 
     if request.GET:
 
-        if category in request.GET:
-            categories = request.GET['category'].split('')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category__name__iexact=category)
+            category = Category.objects.filter(name__in=category)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -33,7 +33,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
+        'current_category': category,
     }
     return render(request, 'products/products.html', context)
 
