ba8747f6d54451c451de881b2728625962e3a738
h-bails <>
2023-08-20 15:26:35 +0000

    Increment bag in top nav when item is added

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,19 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    bag = request.session.get('bag', [])
+
+    for item_id in bag:
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price
+        bag_items.append(product)
 
     if total < settings.FREE_SHIPPING_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -20,7 +27,6 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_SHIPPING_THRESHOLD,
