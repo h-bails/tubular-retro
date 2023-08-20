b8fada35fbab5631eae1b3979e01b527f1813d88
h-bails <>
2023-08-20 15:03:27 +0000

    Add 'add to bag' functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +8,19 @@ def view_bag(request):
     """ A view to return the shopping bag page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add an item to the shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', [])
+
+    if item_id in bag:
+        messages.error(request, "This item is already in your bag.")
+    else:
+        bag.append(item_id)
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
