23c3c7e7912b419817599f25ef5e387c466dede2
h-bails <>
2023-08-20 14:21:04 +0000

    Add shopping bag contexts

--- tubular/settings.py
+++ tubular/settings.py
@@ -119,6 +119,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -187,6 +188,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_SHIPPING_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
