a38416e11118aebdc6957bca6b7f288784410d7c
h-bails <>
2023-08-21 18:54:52 +0000

    Add remove from bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
 # Create your views here.
@@ -20,6 +20,24 @@ def add_to_bag(request, item_id):
         messages.error(request, "This item is already in your bag.")
     else:
         bag.append(item_id)
+        messages.success(request, "Item added to your bag.")
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def remove_from_bag(request, item_id):
+    """ Remove an item from the shopping bag """
+
+    bag = request.session.get('bag', [])
+    try:
+        if item_id in bag:
+            bag.remove(item_id)
+            messages.success(request, "Item removed from your bag.")
+        else:
+            messages.error(request, "This item is not in your bag.")
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
