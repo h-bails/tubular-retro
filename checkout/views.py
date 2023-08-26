6b9c234e10baaae02934a03ea2133e70377d747f
h-bails <>
2023-08-26 14:02:06 +0000

    Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NjMY1GmOOLi0vexTzVo24j0xbH1wOuyl9ngI90WWyUwSCClOL6U82Hgt4qIFXiAbONU3LNi4JA4gQKVqB3wDjqX009njjDqKy',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
