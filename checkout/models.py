3628043ce9fd261a1ea156b01e58dfe5bb106bb0
h-bails <>
2023-08-26 15:39:03 +0000

    Fix form save bug on checkout & add checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         """ Update grand total each time a line item is added, accounting for 
         delivery costs. """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
-            'lineitem_total__sum']
+            'lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_SHIPPING_THRESHOLD:
             self.delivery_cost = self.order_total * \
                 settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -48,6 +48,7 @@ class Order(models.Model):
          it hasn't been set already. """
         if not self.order_number:
             self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
