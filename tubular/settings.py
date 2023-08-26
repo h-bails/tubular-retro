5e590a35fd43fa603af47403ea04020b966f95e0
h-bails <>
2023-08-26 13:22:56 +0000

    Add checkout views and templates

--- tubular/settings.py
+++ tubular/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 AUTHENTICATION_BACKENDS = (
@@ -100,6 +101,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tubular.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -114,8 +117,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
