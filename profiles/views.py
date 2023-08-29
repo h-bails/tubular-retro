72bbdc100f700b2e77823d13b2bea7c53b5c9f1c
h-bails <>
2023-08-29 19:09:27 +0000

    Updated profile view and template

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,13 @@ def profile(request):
     """
     Display the user's profile
     """
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully.')
+
     profile = get_object_or_404(UserProfile, user=request.user)
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -15,6 +22,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
