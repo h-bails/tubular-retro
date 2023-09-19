3bb8b7971b91da33c6d262206d3f1669a4c444c9
h-bails <>
2023-09-19 14:22:09 +0000

    Add sold out notice to sold products

--- checkout/views.py
+++ checkout/views.py
@@ -79,7 +79,8 @@ def checkout(request):
                     order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
+                        "One of the products in your bag wasn't found in\
+                            our database."
                         "Please contact us for assistance.")
                     )
                     order.delete()
